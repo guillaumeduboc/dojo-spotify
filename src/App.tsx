af6632ff2ced9edb5360b3bc43d55f868b2449bb
Thomas Bianco <thomasb@theodo.fr>
2023-04-05 19:10:43 +0200

    level pro

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import { PlaylistTrack } from 'spotify-types';
+import { useQuery } from 'react-query';
+import { useState } from 'react';
+
+const apiToken = '';
+
+export const fetchTracks = async () => {
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Fetching tracks failed with status ${response.status}`);
+  }
+  const data = (await response.json()) as { items: PlaylistTrack[] };
+
+  return data.items;
+};
 
 const App = () => {
+  const {
+    data: tracks,
+    isSuccess,
+    isLoading,
+  } = useQuery('tracks', fetchTracks);
+
+  const [selectedTrackIndex, setSelectedTrackIndex] = useState(0);
+
+  const goToNextTrack = () => {
+    if (isSuccess) {
+      setSelectedTrackIndex((selectedTrackIndex + 1) % tracks?.length);
+    }
+  };
+
+  const selectedTrack = tracks?.[selectedTrackIndex]?.track;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,7 +46,19 @@ const App = () => {
         <h1 className="App-title">Bienvenue sur le blind test</h1>
       </header>
       <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
+        {isLoading || !isSuccess ? (
+          'Loading...'
+        ) : (
+          <div>
+            <h2>{`${selectedTrack?.name} (${selectedTrackIndex + 1} / ${
+              tracks.length
+            })`}</h2>
+            <div>
+              <audio src={selectedTrack?.preview_url ?? ''} controls autoPlay />
+            </div>
+            <button onClick={goToNextTrack}>Next track</button>
+          </div>
+        )}
       </div>
       <div className="App-buttons"></div>
     </div>
